b2fca775dfb1537d71761f76e825e3224d79b75a
yinmengmeng <1062911708@qq.com>
2019-04-23 16:26:39 +0800

    初始化配置

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render("hello", document.querySelector("#root"));
---
+++ webpack.config.dev.js
@@ -0,0 +1,32 @@
+let path = require("path");
+let htmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve("./dist"), //path必须是绝对路径
+        filename: "build.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"]
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: "url-loader"
+            }
+        ]
+    },
+    devtool: "source-map",
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./index.html"
+        })
+    ]
+};

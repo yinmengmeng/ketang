82dbb4ce6eddf8e063dbce737039a5bc15196b52
yinmengmeng <1062911708@qq.com>
2019-04-24 11:22:12 +0800

    react-transition-group实现过渡动画

--- src/containers/Home/HomeHeader.js
+++ src/containers/Home/HomeHeader.js
@@ -1,5 +1,21 @@
 import React, { Component } from "react";
 import logo from "@/common/images/logo.png";
+import { Transition } from "react-transition-group";
+
+//配置Transition参数
+const duration = 300;
+const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+    display: "none"
+};
+const transitionStyles = {
+    entered: { opacity: 1 },
+    exited: {
+        opacity: 0
+    }
+};
+
 export default class HomeHeader extends Component {
     constructor() {
         super();
@@ -13,23 +29,46 @@ export default class HomeHeader extends Component {
         });
     };
     render() {
+        let menuShow = this.state.menuShow;
+        let menuList = (
+            <Transition
+                in={this.state.menuShow}
+                timeout={duration}
+                onEnter={node => {
+                    node.style.display = "block";
+                }}
+                onExited={node => {
+                    node.style.display = "none";
+                }}>
+                {state => (
+                    <div
+                        style={{
+                            ...defaultStyle,
+                            ...transitionStyles[state]
+                        }}>
+                        <ul className="header-menu">
+                            <li>全部课程</li>
+                            <li>React课程</li>
+                            <li>Vue课程</li>
+                        </ul>
+                    </div>
+                )}
+            </Transition>
+        );
+        console.log(menuShow);
         return (
             <div className="home-header">
                 <div className="header-logo">
                     <img src={logo} alt="logo" />
                     <div onClick={this.handleChangeShow}>
-                        {this.state.menuShow ? (
+                        {menuShow ? (
                             <i className="iconfont icon-shanchu1" />
                         ) : (
                             <i className="iconfont icon-liebiao" />
                         )}
                     </div>
-                    <ul className="header-menu">
-                        <li>全部课程</li>
-                        <li>React课程</li>
-                        <li>Vue课程</li>
-                    </ul>
                 </div>
+                {menuList}
             </div>
         );
     }
